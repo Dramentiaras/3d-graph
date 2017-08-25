4031d9c050c41683c47e9f3702b51c83a17ecc3c
Dramentiaras <martin.jirlow@gmail.com>
2017-08-26 00:31:29 +0200

    Moved all js/jsx into src folder

--- src/components/viewport.jsx
+++ src/components/viewport.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import startGrapher from '../grapher/main.js';
+
 export default class GraphViewport extends React.Component {
 
     render() {
         return (<canvas id="webgl_canvas"></canvas>);
+    }
+
+    componentDidMount() {
+
+        startGrapher('webgl_canvas');
     }
 }
